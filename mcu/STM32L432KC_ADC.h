8b5b1fd0bdacf7bedea3b39f61f996e5b0fd0ea6
Erin Wang <156858848+erinlywang@users.noreply.github.com>
2025-12-01 14:32:04 -0800

    Create STM32L432KC_ADC.h for ADC functionality
    Added header file for ADC functions including prototypes.

---
+++ mcu/STM32L432KC_ADC.h
@@ -0,0 +1,19 @@
+// STM32L432KC_ADC.h
+// Header for ADC functions
+
+#ifndef STM32L4_ADC_H
+#define STM32L4_ADC_H
+
+#include <stdint.h>
+#include <stm32l432xx.h>
+
+///////////////////////////////////////////////////////////////////////////////
+// Function prototypes
+///////////////////////////////////////////////////////////////////////////////
+
+void initADC(void);
+
+uint16_t readADC(void);
+
+
+#endif
